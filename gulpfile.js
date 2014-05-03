6d7e293130d3df3a705f52da33f4f255150855de
Alan deLevie <adelevie@gmail.com>
2014-05-03 17:02:32 -0400

    minifies js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require('gulp');
 var react = require('gulp-react');
+var uglify = require('gulp-uglify');
+var gutil = require('gulp-util');
+
+// comment the next line if you don't want the js to be minified.
+gutil.env.type = 'production';
 
 gulp.task('default', function () {
   return gulp.src('src/app.jsx')
     .pipe(react())
+    .pipe(gutil.env.type === 'production' ? uglify({outSourceMap: true}) : gutil.noop())
     .pipe(gulp.dest('build'));
 });
